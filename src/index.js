1ca2ee3e589044f9f47bf570c5190c14f04436dd
daSpinelli <denny.dealmeida@gmail.com>
2020-08-01 13:36:58 -0300

    Padronização de Elementos

--- src/index.js
+++ src/index.js
@@ -2,20 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Home from './pages/home';
-import CadastroVideo from './pages/cadastro/video';
-import CadastroCategoria from './pages/cadastro/categoria';
+import NewVideo from './pages/cadastro/video';
+import NewCategory from './pages/cadastro/categoria';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-const Pagina404 = () => (<div>Página 202 * 2</div>)
+const NotFound = () => (<div>Página 202 * 2</div>)
 
 ReactDOM.render(
   <BrowserRouter>
 
     <Switch>
       <Route path = "/" component = {Home} exact />
-      <Route path = "/cadastro/video" component = {CadastroVideo} />
-      <Route path = "/cadastro/categoria" component = {CadastroCategoria} />
-      <Route component = {Pagina404} />
+      <Route path = "/cadastro/video" component = {NewVideo} />
+      <Route path = "/cadastro/categoria" component = {NewCategory} />
+      <Route component = {NotFound} />
     </Switch>
 
   </BrowserRouter>,
